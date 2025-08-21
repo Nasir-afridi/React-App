02dcfbea98bf7f044eee0423b5c79fe09ca40516
Nasir-afridi <nasirwc05@gmail.com>
2025-08-21 17:03:38 +0500

    Creating Component inside app.jsx file and reuse it in App function

--- React-App/src/App.jsx
+++ React-App/src/App.jsx
@@ -1,9 +1,14 @@
-function App() {
-  return (
-    <>
-      <h1>hello</h1>
-    </>
-  )
+const Name = () => {
+  return <div><h1>hello ggggg</h1></div>
 }
+
+const App = () => {
+  return <div>
+    <h1>hello</h1>
+    <Name />
+  </div>
+}
+
+
 
 export default App
