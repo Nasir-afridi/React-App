dee6d4c00714292b01aacbe8e3e62e66577f680f
Nasir-afridi <nasirwc05@gmail.com>
2025-08-23 17:26:48 +0500

    see how useState working with examples

---
+++ React-App/src/Hooks/1-UseState.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'
+
+const UseState = () => {
+    const [count, setCount] = useState(0);
+    const [type, setType] = useState('')
+    
+  return (
+    <> 
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>Increment</button> <br /><br /><br />
+
+        <input type="text" onChange={(e) => setType(e.target.value)} value={type}/>
+        <p>You typed : {type}</p>
+    </>
+   
+  )
+}
+
+export default UseState
