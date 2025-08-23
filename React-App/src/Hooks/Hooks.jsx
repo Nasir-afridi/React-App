a74e7815a39a47bf899606d2ce572b75d97bcc7e
Nasir-afridi <nasirwc05@gmail.com>
2025-08-23 17:30:34 +0500

    import the useEffect hook

--- React-App/src/Hooks/Hooks.jsx
+++ React-App/src/Hooks/Hooks.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import UseState from './1-UseState'
-
+import UseEffect from './2-UseEffect'
 const Hooks = () => {
   return (
-    <UseState />
+    <>
+        <UseState />
+        <UseEffect />
+    </>
+    
   )
 }
 
