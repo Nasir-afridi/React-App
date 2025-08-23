38ddf6e9539b87e7f7369084da6fd815e7b76a67
Nasir-afridi <nasirwc05@gmail.com>
2025-08-23 18:24:11 +0500

    changes added

--- React-App/src/Hooks/Hooks.jsx
+++ React-App/src/Hooks/Hooks.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import UseState from './1-UseState'
-import UseEffect from './2-UseEffect'
 const Hooks = () => {
   return (
     <>
         <UseState />
-        <UseEffect />
     </>
     
   )
